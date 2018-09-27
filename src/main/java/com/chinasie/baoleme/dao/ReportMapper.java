14f7306900f73ebee75dfcc02d20398286948ede
yuanxiao22 <1024458642@qq.com>
2018-09-27 11:19:12 +0800

    更新

--- src/main/java/com/chinasie/baoleme/dao/ReportMapper.java
+++
@@ -1,9 +0,0 @@
-package com.chinasie.baoleme.dao;
-
-import com.chinasie.baoleme.domain.Report;
-
-public interface ReportMapper {
-    int insert(Report record);
-
-    int insertSelective(Report record);
-}
